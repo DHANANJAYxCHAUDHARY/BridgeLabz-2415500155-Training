46228a29c6b1e1bc491e1e189a4b4b42f56c379a
Dhananjay chaudhary <dhananjay.chaudhary_cs.aiml24@gla.ac.in>
2025-08-28 18:38:05 +0530

    Update 11.java

--- JAVA CONTROL FLOW LEVEL -2/11.java
+++ JAVA CONTROL FLOW LEVEL -2/11.java
@@ -1,8 +1,15 @@
-number = int(input("Enter a positive number less than 100: "))
-if number <= 0 or number >= 100:
-    print("Enter a positive number less than 100.")
-else:
-    print(f"Multiples of {number} below 100 are:")
-    for i in range(100, 0, -1):
-        if i % number == 0:
-            print(i)
+import java.util.Scanner;
+
+public class Program11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0 && number < 100) {
+            for (int i = 100; i >= 1; i--) {
+                if (i % number == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
