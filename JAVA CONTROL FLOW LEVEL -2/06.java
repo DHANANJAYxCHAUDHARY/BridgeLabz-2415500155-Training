6a3ddb6ae433f1faacf2591f6d658076237e8281
Dhananjay chaudhary <dhananjay.chaudhary_cs.aiml24@gla.ac.in>
2025-08-28 18:36:06 +0530

    Update 06.java

--- JAVA CONTROL FLOW LEVEL -2/06.java
+++ JAVA CONTROL FLOW LEVEL -2/06.java
@@ -1,30 +1,19 @@
-# Input ages
-amar_age = int(input("Enter Amar's age: "))
-akbar_age = int(input("Enter Akbar's age: "))
-anthony_age = int(input("Enter Anthony's age: "))
+import java.util.Scanner;
 
-# Input heights
-amar_height = float(input("Enter Amar's height: "))
-akbar_height = float(input("Enter Akbar's height: "))
-anthony_height = float(input("Enter Anthony's height: "))
+public class Program6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age1 = sc.nextInt();
+        double height1 = sc.nextDouble();
+        int age2 = sc.nextInt();
+        double height2 = sc.nextDouble();
+        int age3 = sc.nextInt();
+        double height3 = sc.nextDouble();
 
-# Find youngest
-youngest_age = min(amar_age, akbar_age, anthony_age)
-if youngest_age == amar_age:
-    youngest = "Amar"
-elif youngest_age == akbar_age:
-    youngest = "Akbar"
-else:
-    youngest = "Anthony"
+        int youngest = Math.min(age1, Math.min(age2, age3));
+        double tallest = Math.max(height1, Math.max(height2, height3));
 
-# Find tallest
-tallest_height = max(amar_height, akbar_height, anthony_height)
-if tallest_height == amar_height:
-    tallest = "Amar"
-elif tallest_height == akbar_height:
-    tallest = "Akbar"
-else:
-    tallest = "Anthony"
-
-print(f"The youngest friend is {youngest} with age {youngest_age}.")
-print(f"The tallest friend is {tallest} with height {tallest_height}.")
+        System.out.println("Youngest age: " + youngest);
+        System.out.println("Tallest height: " + tallest);
+    }
+}
